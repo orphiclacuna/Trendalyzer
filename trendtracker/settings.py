43ab194c7373b7b5c75c443ca87ffda0d6dfeb01
Anushka <spacetasticnova@gmail.com>
2025-08-02 10:06:11 +0530

    Added Render URL

--- trendtracker/settings.py
+++ trendtracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+ALLOWED_HOSTS = ['https://trendalyzer.onrender.com', os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 
 # Application definition
